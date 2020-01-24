03665853b95e46775406b6b1f9ce1318d56f09b5
Jesse Niininen <findarkside@gmail.com>
2020-01-24 14:34:30 +0200

    Implement cell and some map utility methods

---
+++ src/main/java/Tiralabra/Cell.java
@@ -0,0 +1,37 @@
+package Tiralabra;
+
+public class Cell {
+
+    private int x, y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!obj.getClass().equals(Cell.class)) {
+            return false;
+        }
+        Cell cell = (Cell) obj;
+        return cell.x == x && cell.getY() == y;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.x;
+        hash = 59 * hash + this.y;
+        return hash;
+    }
+
+}
--- src/main/java/Tiralabra/Map.java
+++ src/main/java/Tiralabra/Map.java
@@ -3,21 +3,37 @@ package Tiralabra;
 import java.util.BitSet;
 
 public class Map {
-    
+
     private int width, height;
     private BitSet map;
-    
+
     public Map(int width, int height) {
         this.width = width;
         this.height = height;
         map = new BitSet(width * height);
     }
-    
-    public boolean getBlock(int x, int y) {
+
+    public boolean isCellBlocked(Cell cell) {
+        return isCellBlocked(cell.getX(), cell.getY());
+    }
+
+    public boolean isCellBlocked(int x, int y) {
         return map.get(x + y * width);
     }
-    
+
+    public boolean isInBounds(int x, int y) {
+        return x > 0 && y > 0 && x < width && y < height;
+    }
+
     public void setBlock(int x, int y, boolean blocked) {
         map.set(x + y * width);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
