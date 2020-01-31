84468f10e2bd1af2227d44df5d6eb4d945d660f2
Jesse Niininen <findarkside@gmail.com>
2020-02-01 01:21:32 +0200

    Revert "Fix JPS prev map having cycle (sometimes)"
    This reverts commit 35910af2e5b065cccb0fd0dc0ced569f8695caab.

--- src/main/java/Tiralabra/pathfinders/JPSPathfinder.java
+++ src/main/java/Tiralabra/pathfinders/JPSPathfinder.java
@@ -81,12 +81,14 @@ public class JPSPathfinder extends Pathfinder {
 
         while (!queue.isEmpty()) {
             PathNode currentNode = queue.poll();
+            System.out.println(currentNode.cell);
             closed.add(currentNode);
             if (bestDist.containsKey(currentNode.cell) && bestDist.get(currentNode.cell) < currentNode.dist) {
                 continue;
             }
 
             if (currentNode.cell.equals(goal)) {
+                System.out.println("Found path " + currentNode.dist);
                 return reconstructPath(start, goal, prev);
             }
 
@@ -133,16 +135,12 @@ public class JPSPathfinder extends Pathfinder {
 
             var diagonalJumpPoints = getJumpPoints(map, currentCell, dx, dy, dist, goal);
             if (!diagonalJumpPoints.isEmpty()) {
-                if (bestDist.getOrDefault(diagonalJumpPoints.get(0), Integer.MAX_VALUE) < dist + 2) {
-                    queue.addAll(diagonalJumpPoints);
-                    prev.put(diagonalJumpPoints.get(0).cell, currentCell);
-                    if (!currentCell.equals(node.cell)) {
-                        prev.put(currentCell, node.cell);
-                    }
-                    return;
+                queue.addAll(diagonalJumpPoints);
+                prev.put(diagonalJumpPoints.get(0).cell, currentCell);
+                if (!currentCell.equals(node.cell)) {
+                    prev.put(currentCell, node.cell);
                 }
-
-                
+                return;
             }
 
             currentCell = new Cell(currentCell.getX() + dx, currentCell.getY() + dy);
