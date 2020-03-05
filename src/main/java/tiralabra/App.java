514bc2740cc67d159fcf54427d8200d793e4871f
Jesse Niininen <findarkside@gmail.com>
2020-03-05 03:48:43 +0200

    Read benchmark data path from args

--- src/main/java/tiralabra/App.java
+++ src/main/java/tiralabra/App.java
@@ -3,16 +3,18 @@
  */
 package tiralabra;
 
-import tiralabra.benchmark.ScenarioLoader;
 import tiralabra.benchmark.BenchmarkRunner;
-import tiralabra.pathfinders.BFSPathfinder;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 public class App {
-    
+
     public static void main(String[] args) throws IOException {
-        BenchmarkRunner.runBencmark(Paths.get("C:\\Users\\FINDarkside\\Downloads\\benchmark"));
+        if (args.length < 1) {
+            System.out.println("Argument for benchmark data location is required");
+            System.exit(0);
+        }
+        BenchmarkRunner.runBencmark(Paths.get(args[0]));
     }
-    
+
 }
